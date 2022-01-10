11a8aa5779e41ac2ce4b5ee8321712d9b24eb192
petrikov.andrey <petrikov.andrey@yandex.ru>
2022-01-10 22:06:10 +0200

    eslint

--- src/test.js
+++ src/test.js
@@ -1,13 +1,13 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require('puppeteer');
 
-describe("Puppeteer test cases", () => {
+describe('Puppeteer test cases', () => {
   beforeAll(async () => {
-    await page.click(".btn");
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.click('.btn');
   });
-  it("popover", async () => {
-    const elem = document.querySelector(".popover");
-    await expect(elem).resolves.toMatch(
-      '<div class="popover"><div class="arrow" style="top: 34px;"></div><h3 class="popover-header">Popover title</h3><div class="popover-body">And here\'s some amazing content. It\'s very engaging. Right?</div></div>'
-    );
+  it('popover', async () => {
+    const elem = document.querySelector('.popover');
+    await expect(elem).resolves.toMatch('<div class="popover"><div class="arrow" style="top: 34px;"></div><h3 class="popover-header">Popover title</h3><div class="popover-body">And here\'s some amazing content. It\'s very engaging. Right?</div></div>');
   });
 });
