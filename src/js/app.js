539525f958ba40dddfa1030559afa0be89278eef
petrikov.andrey <petrikov.andrey@yandex.ru>
2022-01-10 21:48:40 +0200

    test

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,10 @@
 const btn = document.querySelector('.btn');
 btn.addEventListener('click', (e) => {
-  // e.preventDefault();
-  btn.insertAdjacentHTML('beforebegin', '<div class="popover"><div class="arrow" style="top: 34px;"></div><h3 class="popover-header">Popover title</h3><div class="popover-body">And here\'s some amazing content. It\'s very engaging. Right?</div></div>');
+  e.preventDefault();
+  const elem = document.querySelector('.popover');
+  if (elem != null) {
+    elem.remove();
+  } else {
+    btn.insertAdjacentHTML('beforebegin', '<div class="popover"><div class="arrow" style="top: 34px;"></div><h3 class="popover-header">Popover title</h3><div class="popover-body">And here\'s some amazing content. It\'s very engaging. Right?</div></div>');
+  }
 });
--- src/test.js
+++ src/test.js
@@ -1,7 +1,13 @@
-import importScript from './js/app';
+const puppeteer = require("puppeteer");
 
-test('start', () => {
-  const result = importScript.startmain();
-
-  expect(result).toBe(result);
+describe("Puppeteer test cases", () => {
+  beforeAll(async () => {
+    await page.click(".btn");
+  });
+  it("popover", async () => {
+    const elem = document.querySelector(".popover");
+    await expect(elem).resolves.toMatch(
+      '<div class="popover"><div class="arrow" style="top: 34px;"></div><h3 class="popover-header">Popover title</h3><div class="popover-body">And here\'s some amazing content. It\'s very engaging. Right?</div></div>'
+    );
+  });
 });
